4d93f3c5d644bc7bd166fbd5ecc5f4501fbb8159
mirae0312 <ekdtls58@gmail.com>
2021-10-25 20:08:38 +0900

    2 333333 4

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -10,9 +10,8 @@ public class HelloGit {
 	}
 	
 	private void merge() {
-		System.out.println(1);
 		System.out.println(2);
-		System.out.println(3);
+		System.out.println(333333);
 		System.out.println(4);
 
 	}
