092235dd732eba850926328c95625faaeca0db03
mirae0312 <ekdtls58@gmail.com>
2021-10-25 19:26:17 +0900

    hello master 추가!

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		System.out.println("hello web");
-		
+		System.out.println("hello master");
 	}
 
 }
