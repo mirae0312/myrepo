9fe373595d01f27cd730a8ab058f4b70c0192b21
Mirae Kim <77021071+mirae0312@users.noreply.github.com>
2021-10-25 21:17:07 +0900

    원격 저장소에서 작성 출력문 추가

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -12,6 +12,8 @@ public class HelloGit {
 	private void merge() {
 		System.out.println(2);
 		System.out.println(333);
+		System.out.println("이제 github repo에서 관리한다!");
+		System.out.println("원격 저장소에서 작성!");
 	}
 
 }
