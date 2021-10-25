0bea272be85c8467e2f17dd4eec065f88ba338ce
mirae0312 <ekdtls58@gmail.com>
2021-10-25 18:13:17 +0900

    hello world 제거!

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -3,7 +3,6 @@ package com.kh.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
 		System.out.println("hello git");
 		System.out.println("hello web");
 		
