31e60c18a96dbc2b09d990c26f2b6140e32b92b3
Mirae Kim <77021071+mirae0312@users.noreply.github.com>
2021-10-25 21:24:09 +0900

    풀 메세지

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -14,6 +14,7 @@ public class HelloGit {
 		System.out.println(333);
 		System.out.println("이제 github repo에서 관리한다!");
 		System.out.println("원격 저장소에서 작성!");
+		System.out.println("한번에 풀해주세요");
 	}
 
 }
